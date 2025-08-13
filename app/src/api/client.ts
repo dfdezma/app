8a88f67ca1d5c85b90c14056e5ca077f4ae58004
dfdezma <d.fernandezma.2023@alumnos.urjc.es>
2025-08-13 17:45:08 +0200

    Update client.ts

--- app/src/api/client.ts
+++ app/src/api/client.ts
@@ -1,1 +1,16 @@
+import axios from "axios";
+import Constants from "expo-constants";
 
+const apiUrl =
+  (Constants.expoConfig?.extra as { apiUrl: string })?.apiUrl ||
+  "http://localhost:8000";
+
+const client = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 10000, // 10 segundos
+});
+
+export default client;
